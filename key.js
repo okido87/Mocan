0889c4e25171431774b1cc68bc15ff693be2ef54
PoalNinh <ninhphuoc@phuocnv.io.vn>
2025-07-30 16:00:40 +0700

    Create key.js

---
+++ key.js
@@ -0,0 +1,8 @@
+ // API Configuration
+        const API_CONFIG = {
+            APP_ID: '323b6578-b51a-425a-9775-c84d2d8d0904',
+            ACCESS_KEY: 'V2-MipF3-d6wQk-t6AIN-zzz0j-HglL4-Uq2Yp-EckHF-m3lOV',
+            REGION: 'www',
+            BASE_URL: 'https://www.appsheet.com/api/v2/apps',
+            APP_NAME: 'MocAnSystem-777046908'
+        };
